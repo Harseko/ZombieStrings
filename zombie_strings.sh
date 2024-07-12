3c1896a9d052a3b51f9536bcd4cfbeeaf3f87ba1
Nikita Harseko <n.harseko@mail.ru>
2024-07-12 12:29:14 +0200

    Improve optimization script
    This pull request includes updates to the zombie_strings.sh script, which is used to find unused keys in localization files. The changes improve the script's functionality and efficiency.
    
    Changes:
    The script now collects all keys from the .strings files into an array, keys. This allows for more efficient checking of key usage later in the script.
    
    These changes make the script more useful and efficient for finding zombie strings.

--- zombie_strings.sh
+++ zombie_strings.sh
@@ -5,19 +5,27 @@ if [[ $1 ]]; then
   rootFolder=$1
 fi
 
+keys=()
+
 allLocalisableFiles=$(find $rootFolder -type f -name "*.strings");
 for localisableFile in $allLocalisableFiles; do
-  echo -e "\n🔎  Inspecting:" $localisableFile
+  echo "🔎  Inspecting:" $localisableFile
 
   while read p; do
     IFS=" = ";
     string=($p);
     key=${string[0]}
     if [[ ${#string[@]} -gt 1 ]] && [[ $key == \"* ]]; then
-      if ! grep -rsq --include=\*.{swift,m,h} $key $rootFolder; then
-        echo "⚠️ " $key "is not used 💀"
+      if ! echo "${keys[@]}" | grep -q -w "$key"; then
+        keys+=($key)
       fi
     fi
     unset IFS;
   done < $localisableFile
+done
+
+for key in "${keys[@]}"; do
+  if ! grep -rsq --include=\*.{swift,m,h} $key $rootFolder; then
+    echo "⚠️ " $key "is not used 💀"
+  fi
 done
